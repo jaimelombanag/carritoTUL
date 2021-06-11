cbc8c4b3e24c213dab75a843ad04443b438d3c33
alombana24@hotmail.com <alombana24@hotmail.com>
2021-06-11 12:44:24 -0500

    se elimina un model q no se usaba

--- tulshop/lib/src/home_init_controller.dart
+++ tulshop/lib/src/home_init_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
-import 'package:tulshop/src/models/category.dart';
 import 'package:tulshop/src/models/products.dart';
 import 'package:tulshop/src/repository/tul_menu_repository.dart';
 
@@ -10,24 +9,7 @@ class HomeInitController extends ChangeNotifier {
   final TulMenuRepository _foodMenuRepository =
       GetIt.instance<TulMenuRepository>();
 
-  final List<Category> categories = [
-    Category(
-      iconPath: "assets/pages/home/home_tab/breakfast.svg",
-      label: "Breakfast",
-    ),
-    Category(
-      iconPath: "assets/pages/home/home_tab/fries.svg",
-      label: "Fast food",
-    ),
-    Category(
-      iconPath: "assets/pages/home/home_tab/dinner.svg",
-      label: "Dinner",
-    ),
-    Category(
-      iconPath: "assets/pages/home/home_tab/dessert.svg",
-      label: "Desserts",
-    ),
-  ];
+  
 
   List<Products> _popularMenu = [];
   List<Products> get popularMenu => _popularMenu;
